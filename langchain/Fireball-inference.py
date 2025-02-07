8bca075451ca82db805cfa28a12064ea4de21449
Thomas YIu <tom.tyiu@gmail.com>
2025-02-07 14:50:20 -0800

    Update Fireball-inference.py

--- langchain/Fireball-inference.py
+++ langchain/Fireball-inference.py
@@ -1,7 +1,7 @@
 from langchain_huggingface import HuggingFacePipeline
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
-model_id = "EpistemeAI/Fireball-12B"
+model_id = "EpistemeAI/ReasoningCore-3B-0"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(model_id)
 pipe = pipeline(
